bdf6fbe76c0ff3d048bb92030b6da959ce9917c1
turui47 <282265843@qq.com>
2017-09-25 15:38:15 +0800

    Tory
    创建数据库及表的配置

---
+++ Tory/src/main/java/com/tory/weather/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.tory.weather;
+
+import android.app.Application;
+
+/**
+ * Created by soonyo on 2017/9/25.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
