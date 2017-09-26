ee2b70ce2dbbb5c4cf888171f8034f8fb9af8e97
turui47 <282265843@qq.com>
2017-09-26 14:56:32 +0800

    Weather data model.

---
+++ Tory/src/main/java/com/tory/weather/jsonmodle/AQI.java
@@ -0,0 +1,15 @@
+package com.tory.weather.jsonmodle;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
